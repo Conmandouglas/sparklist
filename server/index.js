efcfbc86273ee694df56c8efe0e0dc65e74124d8
Conmandouglas <conmandouglas@gmail.com>
2025-03-23 17:33:12 -0500

    add update, delete, and get specific item routes

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.post('/todos', async (req, res) => {
   }
 })
 
-//get todos
+//get all todos
 app.get('/todos', async (req, res) => {
   try {
     const items = await pool.query("SELECT * FROM items");
@@ -49,6 +49,61 @@ app.get('/todos', async (req, res) => {
   }
 });
 
+//get a specific todo
+//get a specific to do with a number in url
+//get the id from the url
+//get todo using query, selecting all where id is equal
+//send to json
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query(
+      "SELECT * FROM items WHERE item_id = $1",
+      [id]
+    );
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//update a todo
+//get the id from the url
+//get title and content from body
+//make a query for updated todo
+//update items to set the title equal to , and description equal to
+//then sent response saying it was updated
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+  
+    const newTodo = await pool.query(
+      "UPDATE items SET title = $1, content = $2 WHERE item_id = $3",
+      [title, content, id]
+    );
+    res.json("Item has been updated.")
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//delete a todo
+//get id from url
+//query from table where its the id
+//sent response saying it was delted
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const item = await pool.query(
+      "DELETE FROM items WHERE item_id = $1",
+      [id]
+    );
+    res.json('Item has been deleted.')
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 //SERVER SETUP//
 app.listen(port, () => {
